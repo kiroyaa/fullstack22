6ba6b2ca4f06d58a0e56fcbe1943346ee568c0d0
joalhelk <joalhelk@jyu.fi>
2022-07-10 16:29:39 +0300

    authorization to middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -21,10 +21,13 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connection to MongoDB:', error.message)
   })
 
+// use will be executed in order they are in
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
