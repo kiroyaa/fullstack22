46d0f995bcc2a9cab7408fb4e8e904d6fe42dc79
joalhelk <joalhelk@jyu.fi>
2022-07-30 19:57:36 +0300

    added anecdote filtering

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,33 +4,38 @@ import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
+
   const sortedArr = [...anecdotes]
   sortedArr.sort((a, b) => { return a.votes < b.votes })
 
   const vote = (id, content) => {
     dispatch(increaseVotes(id))
     dispatch(setNotification(`you voted '${content}'`))
-
     setTimeout(() => {
       dispatch(setNotification(''))
     }, 5000)
   }
 
+  console.log(filter)
+
   return (
     <div>
       {
-        sortedArr.map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
+        sortedArr
+          .filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+          .map(anecdote =>
+            <div key={anecdote.id}>
+              <div>
+                {anecdote.content}
+              </div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
+              </div>
             </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
-            </div>
-          </div>
-        )
+          )
       }
     </div>
   )
