5321653126de92c2b5d92bd8d8f2b5f50860790f
joalhelk <joalhelk@jyu.fi>
2022-08-04 15:12:54 +0300

    changed to configureStore for thunk middleware

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,21 +1,15 @@
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
-import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
-import anecdoteService from './services/anecdotes'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    notification: notificationReducer,
+    filter: filterReducer
+  }
 })
-
-
-const store = createStore(reducer)
-
-anecdoteService.getAll().then(anecdotes =>
-  store.dispatch(setAnecdotes(anecdotes))
-)
 
 export default store
